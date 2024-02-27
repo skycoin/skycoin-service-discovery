3b97c0f95f71ddaebf11a9684a16d42c95f62a58
Moses Narrow <moe_narrow@use.startmail.com>
2024-02-27 07:45:42 -0600

    fix ci errors

--- cmd/service-discovery/service-discovery.go
+++ cmd/service-discovery/service-discovery.go
@@ -2,10 +2,14 @@
 package main
 
 import (
-	"github.com/skycoin/skycoin-service-discovery/cmd/service-discovery/commands"
+	"log"
+
 	cc "github.com/ivanpirog/coloredcobra"
 	"github.com/spf13/cobra"
+
+	"github.com/skycoin/skycoin-service-discovery/cmd/service-discovery/commands"
 )
+
 func init() {
 	var helpflag bool
 	RootCmd.SetUsageTemplate(help)
@@ -16,13 +20,13 @@ func init() {
 
 func main() {
 	cc.Init(&cc.Config{
-		RootCmd:       commands.RootCmd,
-		Headings:      cc.HiBlue + cc.Bold,
-		Commands:      cc.HiBlue + cc.Bold,
-		CmdShortDescr: cc.HiBlue,
-		Example:       cc.HiBlue + cc.Italic,
-		ExecName:      cc.HiBlue + cc.Bold,
-		Flags:         cc.HiBlue + cc.Bold,
+		RootCmd:         commands.RootCmd,
+		Headings:        cc.HiBlue + cc.Bold,
+		Commands:        cc.HiBlue + cc.Bold,
+		CmdShortDescr:   cc.HiBlue,
+		Example:         cc.HiBlue + cc.Italic,
+		ExecName:        cc.HiBlue + cc.Bold,
+		Flags:           cc.HiBlue + cc.Bold,
 		FlagsDescr:      cc.HiBlue,
 		NoExtraNewlines: true,
 		NoBottomNewline: true,
@@ -32,13 +36,12 @@ func main() {
 	}
 }
 
-	const help = "Usage:\r\n" +
-		"  {{.UseLine}}{{if .HasAvailableSubCommands}}{{end}} {{if gt (len .Aliases) 0}}\r\n\r\n" +
-		"{{.NameAndAliases}}{{end}}{{if .HasAvailableSubCommands}}\r\n\r\n" +
-		"Available Commands:{{range .Commands}}{{if (or .IsAvailableCommand)}}\r\n  " +
-		"{{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableLocalFlags}}\r\n\r\n" +
-		"Flags:\r\n" +
-		"{{.LocalFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasAvailableInheritedFlags}}\r\n\r\n" +
-		"Global Flags:\r\n" +
-		"{{.InheritedFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}\r\n\r\n"
-}
+const help = "Usage:\r\n" +
+	"  {{.UseLine}}{{if .HasAvailableSubCommands}}{{end}} {{if gt (len .Aliases) 0}}\r\n\r\n" +
+	"{{.NameAndAliases}}{{end}}{{if .HasAvailableSubCommands}}\r\n\r\n" +
+	"Available Commands:{{range .Commands}}{{if (or .IsAvailableCommand)}}\r\n  " +
+	"{{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableLocalFlags}}\r\n\r\n" +
+	"Flags:\r\n" +
+	"{{.LocalFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasAvailableInheritedFlags}}\r\n\r\n" +
+	"Global Flags:\r\n" +
+	"{{.InheritedFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}\r\n\r\n"
